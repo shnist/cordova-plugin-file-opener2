c3964da246d2f3f288806860f932b2f9f472e6a1
Aaron Faber <aaron.jack.faber@gmail.com>
2018-12-18 16:51:16 +0000

    test: setting platforms as global env variables

--- tests/travis.sh
+++ tests/travis.sh
@@ -2,19 +2,16 @@
 set -o nounset
 set -o errexit
 
-npm install -g cordova
 npm install
 
 # lint
 npm run eslint
 
 # run tests appropriate for platform
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    sudo gem install cocoapods
-    npm install -g ios-sim ios-deploy
+if [[ "$PLATFORM" == "ios" ]]; then
     npm run test:ios
 fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+if [[ "$PLATFORM" == "android" ]]; then
     echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
     emulator -avd test -no-audio -no-window &
     android-wait-for-emulator
